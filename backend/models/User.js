991f758f9d820fea7774a2c9cdddc19a0a327639
Sujan Butani <sujanbutani30@gmail.com>
2024-10-22 03:55:09 +0530

    E-library Management App

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false }, // Admin users can manage books
+  isAdmin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
